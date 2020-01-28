7e682756c8f7290f1d8f331226534f0f198b625a
Pseudocoder Kim <digression99@gmail.com>
2020-01-28 14:23:32 +0900

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 import ReactMarkdown from "react-markdown"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-import Post from "../components/Post"
+import PostListItem from "../components/PostListItem"
+import styled from "styled-components"
+import data from "../data"
 
 const input = `
 # h1
@@ -14,12 +10,34 @@ const input = `
 * This is test h2
 `
 
-const IndexPage = () => (
-  <div>
-    This is test
-    <Post />
-    <ReactMarkdown source={input} />
-  </div>
-)
+console.log("data is : ", data)
+
+const PostList = styled.div`
+  margin-bottom: 1rem;
+`
+
+const IndexPage = () => {
+  const { posts, author, siteMeta, tags } = data
+
+  return (
+    <div>
+      <div>
+        <h4>This is author data</h4>
+        <div>
+          <span>displayName : {author.displayName}</span>
+        </div>
+      </div>
+
+      <div>
+        <h1>This is post list area.</h1>
+        {posts.map(post => (
+          <PostList>
+            <PostListItem {...post} />
+          </PostList>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default IndexPage
