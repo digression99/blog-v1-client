301064262a0fc8eaf51db842cda0e2c1e88105f6
Pseudocoder Kim <digression99@gmail.com>
2020-01-28 13:51:42 +0900

    Add styled components

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,16 @@
 import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  background: red;
+`
 
 const Post = () => {
   return (
-    <div>
+    <Container>
       <h2>This is post title</h2>
       <div>This is post</div>
-    </div>
+    </Container>
   )
 }
 
