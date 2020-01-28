991c6348b63dd63c320576c646457d779c54f371
Pseudocoder Kim <digression99@gmail.com>
2020-01-28 11:32:56 +0900

    Add Post component

---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Post = () => {
+  return (
+    <div>
+      <h2>This is post title</h2>
+      <div>This is post</div>
+    </div>
+  )
+}
+
+export default Post
