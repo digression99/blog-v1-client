591b3fc60e2a4873bd94123c9dad6de32c73a288
Pseudocoder Kim <digression99@gmail.com>
2020-01-28 15:16:09 +0900

    Update PostListItem

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -2,16 +2,37 @@ import React from "react"
 import styled from "styled-components"
 
 const Container = styled.div`
-  background: green;
+  padding: 1.2rem 0.8rem;
+  border: 2px solid red;
+  border-radius: 10px;
+  h3 {
+    margin: 0;
+  }
+
+  .title-box {
+    margin-bottom: 0.8rem;
+  }
+
+  .tag-box {
+    margin-bottom: 0.5rem;
+  }
+
+  .tag {
+    margin-right: 1rem;
+  }
 `
 
 const PostListItem = ({ title, tags, quote, createdAt, content }) => {
   return (
     <Container>
-      <h3>{title}</h3>
-      <div>
+      <div className="title-box">
+        <h3>{title}</h3>
+      </div>
+      <div className="tag-box">
         {tags.map(tag => (
-          <span>{tag.name} </span>
+          <span className="tag" key={tag.id}>
+            {tag.name}{" "}
+          </span>
         ))}
       </div>
       <div>{quote}</div>
