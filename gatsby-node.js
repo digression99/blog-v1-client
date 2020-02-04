08b6cd9d31afe6661bb53ee3c0f1e9aa4e3fc3e8
Pseudocoder Kim <digression99@gmail.com>
2020-02-04 16:28:05 +0900

    Add slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,36 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+
+const getPostsQuery = `
+{
+  allStrapiPost {
+    edges {
+      node {
+        id
+        title
+        content
+        slug
+      }
+    }
+  }
+}
+`
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const results = await graphql(getPostsQuery)
+
+  results.data.allStrapiPost.edges.forEach(({ node }) => {
+    createPage({
+      path: `/${node.slug}`,
+      component: path.resolve(`src/templates/post.js`),
+      context: {
+        id: node.id,
+        slug: node.slug,
+      },
+    })
+  })
+}
